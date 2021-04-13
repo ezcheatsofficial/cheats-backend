311daf679388a0d7e072c863a456e6b45b6128c0
Oniel <51058739+0niel@users.noreply.github.com>
2021-04-14 00:07:47 +0300

    Update documentation

--- app/routes.py
+++ app/routes.py
@@ -40,6 +40,42 @@ def required_params(required):
 
 @app.route('/api/subscribers/<int:user_id>/', methods=["GET"])
 def get_all_user_subscriptions(user_id):
+    """Полчение информации о всех подписках на различные читы у конкретного пользователя
+        ---
+        consumes:
+          - application/json
+
+        parameters:
+          - in: header
+            name: X-Auth-Token
+            type: string
+            required: true
+          - in: path
+            name: user_id
+            type: integer
+            description: ID пользователя на сайте
+
+        responses:
+          200:
+            description: Информация о всех подписках
+            schema:
+              type: object
+              properties:
+                subscriptions:
+                  type: array
+                  items:
+                    type: object
+                    properties:
+                      cheat_id:
+                        type: string
+                        description: ObjectId чита в строковом формате
+                      subscriber:
+                        $ref: '#/definitions/Subscriber'
+          400:
+            schema:
+              $ref: '#/definitions/Error'
+    """
+
     subscriptions = []
     # проходимся по всем коллекциям и ищим подписки у пользователя
     for cheat_id in subscribers_database.list_collection_names():
@@ -58,6 +94,34 @@ def get_all_user_subscriptions(user_id):
 
 @app.route('/api/subscribers/<string:cheat_id>/<int:user_id>/', methods=["GET"])
 def get_user_subscription_by_cheat(cheat_id, user_id):
+    """Полчение информации о подписке пользователя на конкретный чит
+        ---
+        consumes:
+          - application/json
+
+        parameters:
+          - in: header
+            name: X-Auth-Token
+            type: string
+            required: true
+          - in: path
+            name: cheat_id
+            type: string
+            description: ObjectId чита в строковом формате
+          - in: path
+            name: user_id
+            type: integer
+            description: ID пользователя на сайте
+
+        responses:
+          200:
+            description: Информация о подписке
+            schema:
+              $ref: '#/definitions/Subscriber'
+          400:
+            schema:
+              $ref: '#/definitions/Error'
+    """
     if cheat_id not in subscribers_database.list_collection_names():
         return make_response({'status': 'error', 'message': 'Cheat not found'}), 400
 
@@ -257,6 +321,85 @@ def create_new_cheat():
 
 @app.route('/api/subscribers/<string:cheat_id>/<int:skip>/<int:limit>/', methods=["GET"])
 def get_all_cheat_subscribers(cheat_id, skip, limit):
+    """Получение определённого кол-ва подписчиков конкретного чита, начиная с определённой позиции
+        ---
+        definitions:
+          Subscriber:
+            type: object
+            properties:
+              _id:
+                type: string
+                description: ObjectId
+              user_id:
+                type: integer
+                description: Пользовательский ID на сайте
+              start_date:
+                type: string
+                description: Дата начала подписки (дата первой активации) (ISO формат)
+              expire_date:
+                type: string
+                description: Дата окончания подписки (ISO формат)
+              ip_start:
+                type: string
+                description: IP, с которого проводилась первая активация подписки
+              ip_last:
+                type: string
+                description: IP, с которого пользователь последний раз зашёл в чит
+              secret_data:
+                type: string
+                description: Секретный и уникальный ключ пользователя, который не меняется в рамках одного ПК. Например,
+                    HWID + соль в зашифрованном виде
+              last_online_date:
+                type: string
+                description: Дата последнего захода в чит (ISO формат)
+              subscriptions_count:
+                type: integer
+                description: Количество активаций подписки
+              lifetime:
+                type: boolean
+                description: Имеет ли пользователь бесконечную подписку. Если True, то expire_date не важна
+              active:
+                type: boolean
+                description: Активная ли подписка у пользователя или приостановлена. Не используется для проверки
+                    подписки в чите, только для проверки её активности. Если False, то считаем, что пользователь на
+                    данный момент подписку не имеет, если True, то проверяем lifetime и expire_date
+
+        consumes:
+          - application/json
+
+        parameters:
+          - in: header
+            name: X-Auth-Token
+            type: string
+            required: true
+          - in: path
+            name: cheat_id
+            type: string
+            description: ObjectId чита в строковом формате
+          - in: path
+            name: skip
+            type: integer
+            description: Позиция, с которой начать выборку подписчиков. Если 0, то будут выбраны все с начала
+          - in: path
+            name: limit
+            type: integer
+            description: Количество подписчиков для выборки
+
+        responses:
+          200:
+            description: Список подписчиков по выборке
+            schema:
+              type: object
+              properties:
+                subscribers:
+                  type: array
+                  items:
+                    $ref: '#/definitions/Subscriber'
+          400:
+            schema:
+              $ref: '#/definitions/Error'
+    """
+
     # skip - позиция, с которой начинаем выборку
     # limit - кол-во элементов для выборки
     cursor = subscribers_database[cheat_id].find({}).skip(skip).limit(limit)
@@ -270,6 +413,63 @@ def get_all_cheat_subscribers(cheat_id, skip, limit):
 
 @app.route('/api/cheats/', methods=["GET"])
 def get_all_cheats():
+    """Получения списка всех читов
+        ---
+        definitions:
+          Cheat:
+            type: object
+            properties:
+              _id:
+                type: string
+                description: ObjectId
+              title:
+                type: string
+                description: Название чита
+              owner_id:
+                type: integer
+                description: Пользовательский ID владельца на сайте
+              version:
+                type: string
+                description: Строковая версия чита
+              subscribers:
+                type: integer
+                description: Количество подписчиков с активной подпиской
+              subscribers_for_all_time:
+                type: integer
+                description: Количество подписчиков за всё время
+              subscribers_today:
+                type: integer
+                description: Количество новых подписчиков сегодня
+              undetected:
+                type: boolean
+                description: Статус обнаружения чита античитом
+              created_date:
+                type: string
+                description: Дата добавления чита (ISO формат)
+              updated_date:
+                type: string
+                description: Дата последнего обновления чита (ISO формат)
+              status:
+                type: string
+                description: Статус чита. working - работает, on_update - на обновлении, stopped - остановлен
+
+        consumes:
+          - application/json
+
+        responses:
+          200:
+            description: Полный список приватных читов
+            schema:
+              type: object
+              properties:
+                cheats:
+                  type: array
+                  items:
+                    $ref: '#/definitions/Cheat'
+          400:
+            schema:
+              $ref: '#/definitions/Error'
+    """
     cursor = cheats_database['cheats'].find({})
     cheats = []
     for document in cursor:
@@ -283,6 +483,38 @@ def get_all_cheats():
 
 @app.route('/api/cheats/<string:cheat_id>/', methods=["DELETE"])
 def delete_cheat_by_id(cheat_id):
+    """Удаление чита по его ObjectId и коллекцию подписчиков на чит
+    ---
+    consumes:
+      - application/json
+
+    parameters:
+      - in: header
+        name: X-Auth-Token
+        type: string
+        required: true
+      - in: body
+        name: body
+        type: object
+        schema:
+          properties:
+            cheat_id:
+              type: string
+              description: ObjectId чита в строковом формате
+
+    responses:
+      200:
+        description: Статус-код успешного удаления
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+              description: ok status
+      400:
+        schema:
+          $ref: '#/definitions/Error'
+    """
     try:
         cheat = cheats_database['cheats'].find_one({'_id': ObjectId(cheat_id)})
 
